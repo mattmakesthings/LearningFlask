7a717cdc3531f4bf82c2df9c9f00f12611678bb2
mattmakesthings <matt.weidman.codes@gmail.com>
2019-04-03 13:17:50 -0700

    closer

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
-
+db = SQLAlchemy()
 migrate = Migrate()
 
 def create_app(config_class=Config):
@@ -16,7 +16,6 @@ def create_app(config_class=Config):
     return app
 
 app = create_app()
-db = SQLAlchemy(app)
 
 
 from app import routes, models
